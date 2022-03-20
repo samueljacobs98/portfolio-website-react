648c71418b9ddbeb5f52d1bfa11eea6abcace621
Samuel Jacobs <sammyjacobscontact@gmail.com>
2022-03-20 12:30:16 +0000

    restyled about page

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -6,15 +6,13 @@ import sun from "../../assets/svgs/sun-side.svg";
 const About = () => {
   return (
     <div className="about" id="about">
-      <div className="about__content-container">
+      <h2 className="about__item-title">About Me</h2>
+      <div className="about__container">
         <img className="about__img" src={profileImg} alt="" />
-        <div className="about__container">
-          <h2 className="about__item-title">About Me</h2>
-          <div className="about__container-item">
-            <p className="about__item-text">{aboutData.summary1}</p>
-            <br />
-            <p className="about__item-text">{aboutData.summary2}</p>
-          </div>
+        <div className="about__summary-container">
+          <p className="about__item-text">{aboutData.summary1}</p>
+          <br />
+          <p className="about__item-text">{aboutData.summary2}</p>
         </div>
       </div>
       <img className="about__sun" src={sun} alt="the sun" />
